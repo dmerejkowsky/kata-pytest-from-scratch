57fb8fab101ed51e545d8b098d3085b821ba678e
Dimitri Merejkowsky <dimitri@dmerej.info>
2022-06-29 16:10:59 +0200

    Implement framework.collect() and framework.summarize()

--- framework.py
+++ framework.py
@@ -1,5 +1,15 @@
+import sys
+import importlib
+import types
+import inspect
 from enum import Enum
 from dataclasses import dataclass
+
+
+@dataclass
+class Test:
+    name: str
+    function: types.FunctionType
 
 
 class TestStatus(Enum):
@@ -9,18 +19,61 @@ class TestStatus(Enum):
 
 @dataclass
 class Outcome:
+    name: str
     test_status: TestStatus
-    exception: Exception | None
+    exception: Exception | None = None
 
 
-def run(func) -> Outcome:
+def run(test: Test) -> Outcome:
     test_status = None
     exception = None
     try:
-        func()
+        test.function()
         test_status = TestStatus.success
     except Exception as e:
         test_status = TestStatus.failure
         exception = e
 
-    return Outcome(test_status=test_status, exception=exception)
+    return Outcome(name=test.name, test_status=test_status, exception=exception)
+
+
+def collect(module):
+    for name, value in inspect.getmembers(module, inspect.isfunction):
+        if name.startswith("test_"):
+            yield Test(name=name, function=value)
+
+
+@dataclass
+class Summary:
+    total: int
+    errors: int
+
+
+def summarize(outcomes):
+    total = len(outcomes)
+    errors = len([o for o in outcomes if o.test_status != TestStatus.success])
+    return Summary(total=total, errors=errors)
+
+
+def main():
+    name = sys.argv[1]
+    module = importlib.import_module(name)
+    tests = collect(module)
+    outcomes = []
+    for test in tests:
+        outcome = run(test)
+        if outcome.test_status != TestStatus.success:
+            print(test.name)
+            print(type(outcome.exception))
+        outcomes.append(outcome)
+    summary = summarize(outcomes)
+    print(f"Ran {summary.total} tests")
+    if summary.errors:
+        print(f"FAILED (errors={summary.errors})")
+        sys.exit(1)
+    else:
+        print("Ok")
+
+
+if __name__ == "__main__":
+    main()
--- test_framework.py
+++ test_framework.py
@@ -1,28 +1,57 @@
 import unittest
 
 import framework
-from framework import TestStatus
+from framework import TestStatus, Test, Outcome
 
 
-class TestFramework(unittest.TestCase):
+class CollectTestCase(unittest.TestCase):
+    def test_collect_tests_in_modules(self):
+        import test_foo
+        from test_foo import test_bar_ok, test_bar_ko, test_answer
+
+        tests = list(framework.collect(test_foo))
+
+        test_functions = [t.function for t in tests]
+        test_names = [t.name for t in tests]
+
+        self.assertEqual(test_functions, [test_answer, test_bar_ko, test_bar_ok])
+        self.assertEqual(test_names, ["test_answer", "test_bar_ko", "test_bar_ok"])
+
+
+class RunTestCase(unittest.TestCase):
     def test_can_run_sucessful_test(self):
         def test_equality():
             assert 1 + 1 == 2
 
-        outcome = framework.run(test_equality)
+        test = Test(name="test_equality", function=test_equality)
+        outcome = framework.run(test)
         self.assertEqual(outcome.test_status, TestStatus.success)
-
-    def test_can_run_failing_test(self):
-        def failing_test():
-            assert 1 + 1 == 3
-
-        outcome = framework.run(failing_test)
-        self.assertEqual(outcome.test_status, TestStatus.failure)
+        self.assertEqual(outcome.name, "test_equality")
 
     def test_can_catch_exceptions(self):
         def failing_test():
             assert 1 + 1 == 3
 
-        outcome = framework.run(failing_test)
+        test = Test(name="failing_test", function=failing_test)
+
+        outcome = framework.run(test)
+
         self.assertEqual(outcome.test_status, TestStatus.failure)
         self.assertIsInstance(outcome.exception, AssertionError)
+
+
+class ReportTestCase(unittest.TestCase):
+    def test_summary(self):
+        outcomes = [
+            Outcome(name="test_one", test_status=TestStatus.success),
+            Outcome(
+                name="test_two",
+                test_status=TestStatus.failure,
+                exception=KeyError("foo"),
+            ),
+            Outcome(name="test_three", test_status=TestStatus.success),
+        ]
+
+        summary = framework.summarize(outcomes)
+        self.assertEqual(summary.total, 3)
+        self.assertEqual(summary.errors, 0)
